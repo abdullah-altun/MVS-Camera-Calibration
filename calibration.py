c447830e07430e1ad9bd51db1c9e92b35792f151
abdullah <abdul_altun@hotmail.com>
2024-09-27 11:24:13 +0300

    cameraRead düzeltildi
    iki kamerayı paralel şekilde okuma işlemi gerçekleştirildi

--- calibration.py
+++ calibration.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import glob
 import pickle
 
-chessboardSize = (9,6)
+chessboardSize = (14,10)
 frameSize = (3648,5472)
 
 
@@ -12,59 +12,94 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
 
-size_of_chessboard_squares_mm = 20
+size_of_chessboard_squares_mm = 19
 objp = objp * size_of_chessboard_squares_mm
 
 objpoints = []
 imgpoints = []
 
-images = glob.glob('images/camera1/*.png')
+def calibMatrix(images,cameName):
+    workT = False
+    for image in images:
+        img = cv.imread(image)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-for image in images:
-    img = cv.imread(image)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+        if ret == True:
+            workT = True
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners)
 
-    ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
-    if ret == True:
+            cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+            cv.imshow('img', cv.resize(img,(1920,1080)))
+            cv.waitKey(1000)
 
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
+    if workT == False:
+        assert ValueError("Görüntü Matrisi Yok")
 
-        cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(1000)
+    cv.destroyAllWindows()
+    ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
 
-cv.destroyAllWindows()
+    pickle.dump((cameraMatrix, dist), open(f"calibMatrix_{cameName}_calibration.pkl", "wb" ))
+    pickle.dump(cameraMatrix, open(f"calibMatrix_{cameName}_cameraMatrix.pkl", "wb" ))
+    pickle.dump(dist, open(f"calibMatrix_{cameName}_dist.pkl", "wb" ))
 
-ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
 
-pickle.dump((cameraMatrix, dist), open( "calibration.pkl", "wb" ))
-pickle.dump(cameraMatrix, open( "cameraMatrix.pkl", "wb" ))
-pickle.dump(dist, open( "dist.pkl", "wb" ))
+images1 = glob.glob('images/camera1/**')
+images2 = glob.glob("images/camera2/**")
 
-img = cv.imread('images/camera1/3.png')
-h,  w = img.shape[:2]
-newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+calibMatrix(images1,"camera1")
+calibMatrix(images2,"camera2")
 
-dst = cv.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
 
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('caliResult1.png', dst)
+# for image in images:
+#     img = cv.imread(image)
+#     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-mapx, mapy = cv.initUndistortRectifyMap(cameraMatrix, dist, None, newCameraMatrix, (w,h), 5)
-dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+#     ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+#     if ret == True:
 
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('caliResult2.png', dst)
+#         objpoints.append(objp)
+#         corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+#         imgpoints.append(corners)
 
-mean_error = 0
+#         cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+#         cv.imshow('img', cv.resize(img,(1920,1080)))
+#         cv.waitKey(1000)
 
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-    mean_error += error
+# cv.destroyAllWindows()
 
-print( "total error: {}".format(mean_error/len(objpoints)) )
+# ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+
+# pickle.dump((cameraMatrix, dist), open( "calibration.pkl", "wb" ))
+# pickle.dump(cameraMatrix, open( "cameraMatrix.pkl", "wb" ))
+# pickle.dump(dist, open( "dist.pkl", "wb" ))
+
+
+
+# img = cv.imread('images/camera1/Image_20240926130733201.bmp')
+# h,  w = img.shape[:2]
+# newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+
+# dst = cv.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
+
+# x, y, w, h = roi
+# dst = dst[y:y+h, x:x+w]
+# cv.imwrite('caliResult1.png', dst)
+
+# mapx, mapy = cv.initUndistortRectifyMap(cameraMatrix, dist, None, newCameraMatrix, (w,h), 5)
+# dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+
+# x, y, w, h = roi
+# dst = dst[y:y+h, x:x+w]
+# cv.imwrite('caliResult2.png', dst)
+
+# mean_error = 0
+
+# for i in range(len(objpoints)):
+#     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
+#     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+#     mean_error += error
+
+# print( "total error: {}".format(mean_error/len(objpoints)) )
--- cameraRead.py
+++ cameraRead.py
@@ -1,66 +1,85 @@
 from hik_camera.hik_camera import HikCamera
 import cv2
 import os
+from threading import Thread, Lock
+
+lock = Lock()
 
 class GetImages:
-    def __init__(self,cap1,cap2,ips):
+    def __init__(self, cap1, cap2, ips):
         self.cap1 = cap1
         self.cap2 = cap2
         self.ips = ips
+        self.img1 = None
+        self.img2 = None
+        self.thread1 = None
+        self.thread2 = None
+        self.running = True
 
-        self.foldercrate()        
-        
+        self.foldercrate()
+
     def foldercrate(self):
-        if not(os.path.exists("images")):
+        if not os.path.exists("images"):
             os.mkdir("images")
-        if not(os.path.exists("images/camera1")):
+        if not os.path.exists("images/camera1"):
             os.mkdir("images/camera1")
-        if not(os.path.exists("images/camera2")):
+        if not os.path.exists("images/camera2"):
             os.mkdir("images/camera2")
+
+    def capture_images_thread(self, camera, camera_name, cam_idx):
+        while self.running:
+            with camera: 
+                camera["ExposureAuto"] = "Off"
+                camera["ExposureTime"] = 50000
+
+                with lock:
+                    img = camera.robust_get_frame()
+
+                if cam_idx == 1:
+                    self.img1 = img
+                elif cam_idx == 2:
+                    self.img2 = img
+
+    def start_threads(self):
+        self.thread1 = Thread(target=self.capture_images_thread, args=(self.cap1, "Camera 1", 1))
+        self.thread2 = Thread(target=self.capture_images_thread, args=(self.cap2, "Camera 2", 2))
+
+        self.thread1.start()
+        self.thread2.start()
+
+    def stop_threads(self):
+        self.running = False
+        self.thread1.join()
+        self.thread2.join()
 
     def view(self):
         num = 0
+        self.start_threads()  
 
-        with self.cap1:
-            self.cap1["ExposureAuto"] = "Off"
-            self.cap1["ExposureTime"] = 50000
+        while True:
+            if self.img1 is not None and self.img2 is not None:
 
-            if cam2 is not None:
-                with self.cap2:
-                    self.cap2["ExposureAuto"] = "Off"
-                    self.cap2["ExposureTime"] = 50000
+                combined_image = cv2.hconcat([self.img1, self.img2])
+                cv2.imwrite(f"images/combined_{str(num)}.png", combined_image)
 
-                while True:
-                    img1 = self.cap1.robust_get_frame()
-                    img2 = self.cap2.robust_get_frame()
-                    k = cv2.waitKey(1)
-                    if k == 27:
-                        break
-                    elif k == ord("s"):
-                        cv2.imwrite(f"images/camera2/{str(num)}.png",img1)
-                        cv2.imwrite(f"images/camera1/{str(num)}.png",img2)
-                        print("image saved!")
-                        num += 1
-                    
-                    cv2.imshow("camera1",cv2.resize(img1,(1920,1080)))
-                    cv2.imshow("camera2",cv2.resize(img2,(1920,1080)))
+                resized_combined = cv2.resize(combined_image, (1920, 540)) 
+                cv2.imshow("Combined Image", resized_combined)
 
-            else:
-                while True:
-                    img = self.cap1.robust_get_frame()
-                    k = cv2.waitKey(1)
-                    if k == ord("q"):
-                        break
-                    elif k == ord("s"):
-                        cv2.imwrite(f"images/camera1/{str(num)}.png",img)
-                        print("image saved!")
-                        num += 1
-                    cv2.imshow("camera1",cv2.resize(img,(1920,1080)))
-    
+                k = cv2.waitKey(1)
+                if k == 27:
+                    break
+                elif k == ord("s"): 
+                    cv2.imwrite(f"images/camera1/{str(num)}.png", self.img1)
+                    cv2.imwrite(f"images/camera2/{str(num)}.png", self.img2)
+                    print(f"Images from both cameras saved as {num}.png!")
+                    num += 1
+
+        self.stop_threads() 
         cv2.destroyAllWindows()
 
 ips = HikCamera.get_all_ips()
 print("All camera IP addresses:", ips)
+
 if len(ips) == 0:
     raise ValueError("Hiç kamera bulunamadı!")
 elif len(ips) == 1:
@@ -75,10 +94,5 @@ elif len(ips) >= 2:
     cam1 = HikCamera(ip=cam1_ip)
     cam2 = HikCamera(ip=cam2_ip)
 
-
-getImage = GetImages(cam1,cam2,ips)
+getImage = GetImages(cam1, cam2, ips)
 getImage.view()
-
-    
-
-
